46fdafbcdaa167f16e1d0b08fee1d32f9897b5de
mahbub09 <mahabub895alam@gmail.com>
2022-03-26 23:55:04 +0600

    finished

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
+        <div className='header'>
             <h2 className='txt-hdr'>WATCH BURG </h2>
             <hr />
         </div>
